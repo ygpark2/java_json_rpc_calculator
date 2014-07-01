c7617814efb8ff8b7d41e107b0a40d5cf7c06b21
ygpark2 <ygpark2@gmail.com>
2014-07-01 20:34:10 +1000

    Add test case for each cases

--- src/test/java/com/pyg/ain/Calculator.java
+++ src/test/java/com/pyg/ain/Calculator.java
@@ -3,6 +3,8 @@ package com.pyg.ain;
 import static org.junit.Assert.*;
 
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -10,32 +12,95 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Calculator {
-	@Test
-	public void test() {
-        System.out.println("============================");
-        System.out.println(new BigDecimal("343434.342434"));
+	private JsonRpcHttpClient client;
+
+   	@Before
+   	public void setUp() {
         try {
-            JsonRpcHttpClient client = new JsonRpcHttpClient(new URL("http://localhost:8080/calculatorService"));
+			client = new JsonRpcHttpClient(new URL("http://localhost:8080/ain/calculatorService"));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+   	}
 
-            BigDecimal point = client.invoke("point", new Object[] { "34234.4234" }, BigDecimal.class);
-            int plus = client.invoke("plus", new Object[] { 3, 4 }, Integer.class);
-            int minus = client.invoke("minus", new Object[] { 8, 4 }, Integer.class);
-            int multiply = client.invoke("multiply", new Object[] { 3, 4 }, Integer.class);
-            int divide = client.invoke("divide", new Object[] { 8, 2 }, Integer.class);
-
-            System.out.println("point => " + point);
-            System.out.println("sum => " + plus);
-            System.out.println("sum => " + minus);
-            System.out.println("sum => " + multiply);
-            System.out.println("sum => " + divide);
-
+	@Test
+	public void testPoint() {
+        try {
+            BigDecimal point = client.invoke("point", new String[] { "382934.38978284" }, BigDecimal.class);
+            assertEquals(new BigDecimal("382934.38978284"), point);
         } catch (Throwable te) {
-            System.err.println("Caught IOException: " + te.getMessage());
+        	fail("Caught IOException: " + te.getMessage());
         }
-
-
-
-		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlus() {
+        try {
+            int plus = client.invoke("plus", new Object[] { 3, 4 }, Integer.class);
+            assertEquals(7, plus);
+        } catch (Throwable te) {
+            fail("Caught IOException: " + te.getMessage());
+        }
 	}
 
+	@Test
+	public void testMinus() {
+        try {
+            int minus = client.invoke("minus", new Object[] { 8, 4 }, Integer.class);
+            assertEquals(4, minus);
+        } catch (Throwable te) {
+        	fail("Caught IOException: " + te.getMessage());
+        }
+	}
+
+	@Test
+	public void testMultiply() {
+        try {
+            int multiply = client.invoke("multiply", new Object[] { 3, 4 }, Integer.class);
+            assertEquals(12, multiply);
+        } catch (Throwable te) {
+        	fail("Caught IOException: " + te.getMessage());
+        }
+	}
+	
+	@Test
+	public void testDivide() {
+        try {
+            int divide = client.invoke("divide", new Object[] { 8, 2 }, Integer.class);
+            assertEquals(4, divide);
+        } catch (Throwable te) {
+        	fail("Caught IOException: " + te.getMessage());
+        }
+	}
+
+	@Test
+	public void testLog() {
+        try {
+        	double log = client.invoke("log10", new Object[] { 100 }, Integer.class);
+            assertEquals(2, log, 0.001);
+        } catch (Throwable te) {
+        	fail("Caught IOException: " + te.getMessage());
+        }
+	}
+
+	@Test
+	public void testExp() {
+        try {
+            double exp = client.invoke("exp", new Object[] { 8 }, Integer.class);
+            assertEquals(2980.0, exp, 0.1);
+        } catch (Throwable te) {
+        	fail("Caught IOException: " + te.getMessage());
+        }
+	}
+
+	@Test
+	public void testSqrt() {
+        try {
+            double sqrt = client.invoke("sqrt", new Object[] { 64 }, Integer.class);
+            assertEquals(8, sqrt, 0.001);
+        } catch (Throwable te) {
+        	fail("Caught IOException: " + te.getMessage());
+        }
+	}
 }
